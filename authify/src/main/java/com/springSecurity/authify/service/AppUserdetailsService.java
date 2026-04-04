4b8052103d8f450bef63ba55a173255fa59bbb07
Kedar Gajmal <kedargajmal8@gmail.com>
2026-04-04 15:07:49 +0530

    feat: implement AppUserdetailsService to support Spring Security user authentication

--- authify/src/main/java/com/springSecurity/authify/service/AppUserdetailsService.java
+++ authify/src/main/java/com/springSecurity/authify/service/AppUserdetailsService.java
@@ -3,13 +3,14 @@ package com.springSecurity.authify.service;
 import com.springSecurity.authify.entity.UserEntity;
 import com.springSecurity.authify.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
@@ -24,8 +25,10 @@ public class AppUserdetailsService implements UserDetailsService {
         UserEntity existingUser = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Email not found for email: " + email));
 
-        return new User(existingUser.getEmail(), existingUser.getPassword(), 
-                java.util.Collections.singletonList(new org.springframework.security.core.authority.SimpleGrantedAuthority("ROLE_USER")));
+        return new User(existingUser.getEmail(), existingUser.getPassword(), new ArrayList<>());
+               
+        
+        //java.util.Collections.singletonList(new org.springframework.security.core.authority.SimpleGrantedAuthority("ROLE_USER")));
 //            .withUsername(existingUser.getEmail())
 //                .password(existingUser.getPassword())
 //                .authorities("USER")
